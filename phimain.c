2f7f0c6b379c3976fcd02ada9bfa3460bd29ec9d
BeDummer <benjamin.dummer@googlemail.com>
2016-06-15 14:27:13 +0200

    finalised main procedure

--- phimain.c
+++ phimain.c
@@ -40,29 +40,31 @@ double lambda;
   
   double complex M(double complex *phi)
   {
+    int k;
     double complex sum = 0.;
-    for (int k=0; k<nvol; k++)
+    for (k=0; k<nvol; k++)
       sum += phi[k];
     return (sum/((double) nvol));
   }
   
   double P(double S_tmp)
   {
-    return (exp(S_tmp));
+    return (exp(-S_tmp));
   }
   
   void calc_local_dist(double complex *phi, double complex h, double *p)
   {
+    int k;
     int mu;
     double phi_sqr;
     double complex B;
-    for (int k=0; k<nvol; k++)
+    for (k=0; k<nvol; k++)
     {
       phi_sqr = pow(cabs(phi[k]),2);
       B = h;
       for(mu=1; mu<=ndim; mu++)
       {
-	B += kappa*(phi[nn[mu][k]]+phi[nn[ndim+mu][k]])
+	B += kappa*(phi[nn[mu][k]]+phi[nn[ndim+mu][k]]);
       }
       p[k] = exp(conj(B)*phi[k]+B*conj(phi[k])-phi_sqr-lambda*pow((phi_sqr-1),2));
     }
@@ -72,13 +74,13 @@ double lambda;
   {
     	  int k;
 	  /* Intializes random number generator */
-//	  time_t t;
-//	  srand((unsigned) time(&t));
+	  time_t t;
+	  srand((unsigned) time(&t));
 	  
 	  // Parameter
 	  ndim = 2;
-	  kappa = 8;
-	  lambda =16;
+	  kappa = 1.;
+	  lambda =1.;
 	  int N = 10;  
 	  
 	 // Set fuer geom_pbc
@@ -100,18 +102,27 @@ double lambda;
 	  double randreal, randim;
 	for(k=0; k<nvol; k++)
 	{
-		randreal = (double)(rand() & 0xFF ) * .1;
-		randim = (double)(rand() & 0xFF ) * .1;
+		randreal = (double)(rand() & 0xFF ) * .01;
+		randim = (double)(rand() & 0xFF ) * .01;
 		phi[k] = (double complex)randreal + randim*I;
 	}
-	double P_old = P(S(phi,h));
-	calc_local_dist(phi,h,p_old);
-
-	printf("%f\n", P_old);
 	
 	int randpos = (rand() & 0xFF) % nvol;
-	printf("%d\n", randpos);
-
+	printf("Changed Position: %d\n", randpos);
+	
+	double S_old = S(phi,0.);
+	calc_local_dist(phi,0.,p_old);
+	printf("Phi(x)=%f+i*%f  S=%f  p(x)=%e\n", creal(phi[randpos]),cimag(phi[randpos]), S_old, p_old[randpos]);
+	randreal = (double)(rand() & 0xFF ) * .01;
+	randim = (double)(rand() & 0xFF ) * .01;
+	phi[randpos] = (double complex)randreal + randim*I;
+	double S_new = S(phi,0.);
+	calc_local_dist(phi,0.,p_new);
+	printf("Phi'(x)=%f+i*%f  S'=%f  p'(x)=%e\n", creal(phi[randpos]),cimag(phi[randpos]), S_new, p_new[randpos]);
+	
+	printf("P'/P=%e  p'(x)/p(x)=%e\n",exp(S_old-S_new),(p_new[randpos]/p_old[randpos]));
+	double complex M_new = M(phi);
+	printf("M=%f + i*%f\n",creal(M_new),cimag(M_new));
 	
 	free(lsize);
 	free(nn);
